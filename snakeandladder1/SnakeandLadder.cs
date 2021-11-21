53e17adb76d1ccb14d25e68a9adcd5229467c288
lismitha <lismithaspatel@gmail>
2021-11-21 22:31:26 +0530

    adding welcome file

---
+++ snakeandladder1/SnakeandLadder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int StartingPosition = 0;
+            //Console.WriteLine("Starting position " + StartingPosition);
+
+            Random random = new Random();
+            int DiceNum = random.Next(1, 7);
+
+            Console.WriteLine("Dice Number is " + DiceNum);
+
+        }
+    }
+}
