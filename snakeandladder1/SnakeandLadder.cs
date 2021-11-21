e8f60a94dfeef3927b5963f97a2fab275f397209
lismitha <lismithaspatel@gmail>
2021-11-21 23:09:10 +0530

    adding no play file

--- snakeandladder1/SnakeandLadder.cs
+++ snakeandladder1/SnakeandLadder.cs
@@ -6,11 +6,35 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            //int StartingPosition = 0;
+            //Console.WriteLine("Starting position " + StartingPosition);
+
+            int Ladder = 1;
+            int Snake = 2;
+            int CurrentPosition = 0;
+
             Random random = new Random();
             int DiceNumber = random.Next(1, 7);
 
+            Random rand = new Random();
+            int Option = rand.Next(0, 3);
+
             Console.WriteLine("Dice Number is " + DiceNumber);
 
+            if (Option == Ladder)
+            {
+                CurrentPosition = CurrentPosition + DiceNumber;
+                Console.WriteLine("Ladder Current Position " + CurrentPosition);
+            }
+            else if (Option == Snake)
+            {
+                CurrentPosition = CurrentPosition - DiceNumber;
+                Console.WriteLine("Snake Current Position " + CurrentPosition);
+            }
+            else
+            {
+                Console.WriteLine("Their is no play");
+            }
         }
     }
 }
