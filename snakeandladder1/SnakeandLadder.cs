6d91333cd061f28bead810ddc03f4aa904c688b9
lismitha <lismithaspatel@gmail>
2021-11-21 23:18:19 +0530

    when dice reaches abve 100 stop play

--- snakeandladder1/SnakeandLadder.cs
+++ snakeandladder1/SnakeandLadder.cs
@@ -6,34 +6,44 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
-            //int StartingPosition = 0;
-            //Console.WriteLine("Starting position " + StartingPosition);
-
+            int StartingPosition = 0;
             int Ladder = 1;
             int Snake = 2;
             int CurrentPosition = 0;
+            const int WinnigPosition = 100;
 
-            Random random = new Random();
-            int DiceNumber = random.Next(1, 7);
-
-            Random rand = new Random();
-            int Option = rand.Next(0, 3);
-
-            Console.WriteLine("Dice Number is " + DiceNumber);
-
-            if (Option == Ladder)
+            while (CurrentPosition <= WinnigPosition)
             {
-                CurrentPosition = CurrentPosition + DiceNumber;
-                Console.WriteLine("Ladder Current Position " + CurrentPosition);
-            }
-            else if (Option == Snake)
-            {
-                CurrentPosition = CurrentPosition - DiceNumber;
-                Console.WriteLine("Snake Current Position " + CurrentPosition);
-            }
-            else
-            {
-                Console.WriteLine("Their is no play");
+                Random random = new Random();
+                int DiceNumber = random.Next(1, 7);
+
+                Random rand = new Random();
+                int Option = rand.Next(0, 3);
+
+                Console.WriteLine("Dice Number is " + DiceNumber);
+
+                if (Option == Ladder)
+                {
+                    CurrentPosition = CurrentPosition + DiceNumber;
+                    Console.WriteLine("Ladder Current Position " + CurrentPosition);
+                }
+                else if (Option == Snake)
+                {
+                    CurrentPosition = CurrentPosition - DiceNumber;
+                    if (CurrentPosition < StartingPosition)
+                    {
+                        CurrentPosition = StartingPosition;
+                        Console.WriteLine("Snake Current Position " + CurrentPosition);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Snake Current Position " + CurrentPosition);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Their is no play");
+                }
             }
         }
     }
